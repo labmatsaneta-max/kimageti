3e287b1b96d784d6d2788f39aa6ea5037d69dca7
labmatsaneta-max <labmatsaneta@gmail.com>
2026-08-05 14:21:41 +0700

    Update app.js

--- app.js
+++ app.js
@@ -276,7 +276,7 @@ function loadLocalStorage() {
     if (!DB.Setting.script_url) {
         DB.Setting.script_url = 'https://script.google.com/macros/s/AKfycbw5-2THf_vqIAvaM-x2jNGnoVRpBRFEW2wuoSMcao65u0YwsGCSBgaJkjFJYAvbSCI8/exec'; // <-- MASUKKAN URL ANDA JUGA DI SINI
     }
-    document.getElementById('cfg-script-url').value = DB.Setting.script_url || '';
+    document.getElementById('cfg-script-url').value = DB.Setting.script_url || 'https://script.google.com/macros/s/AKfycbw5-2THf_vqIAvaM-x2jNGnoVRpBRFEW2wuoSMcao65u0YwsGCSBgaJkjFJYAvbSCI8/exec';
     document.getElementById('cfg-nama-kbihu').value = DB.Setting.nama_kbihu || 'KBIHU KI MAGETI';
     document.getElementById('cfg-tahun').value = DB.Setting.tahun || '1448 H / 2027 M';
     document.getElementById('cfg-alamat').value = DB.Setting.alamat || '';
