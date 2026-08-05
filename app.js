ca20737612957794772bc6b0884702c1a8a7f49f
labmatsaneta-max <labmatsaneta@gmail.com>
2026-08-05 14:27:06 +0700

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ let DB = {
     Pembayaran: [],
     Jadwal: [],
     Setting: {
-        script_url: 'AKfycbw5-2THf_vqIAvaM-x2jNGnoVRpBRFEW2wuoSMcao65u0YwsGCSBgaJkjFJYAvbSCI8',
+        script_url: 'https://script.google.com/macros/s/AKfycbw5-2THf_vqIAvaM-x2jNGnoVRpBRFEW2wuoSMcao65u0YwsGCSBgaJkjFJYAvbSCI8/exec',
         admin_pass: 'admin123',
         nama_kbihu: 'KBIHU KI MAGETI',
         tahun: '1448 H / 2027 M',
